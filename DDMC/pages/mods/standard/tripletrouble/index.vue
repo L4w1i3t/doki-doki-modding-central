dddebb676df0d8b2596947eb56c79e3b4353b996
L4w1i3t <84912726+L4w1i3t@users.noreply.github.com>
2024-02-21 00:02:30 -0500

    TT DL page

--- DDMC/pages/mods/standard/tripletrouble/index.vue
+++ DDMC/pages/mods/standard/tripletrouble/index.vue
@@ -2,21 +2,25 @@
   <div class="download-page">
     <div class="thumbnail-container">
       <img src="/assets/mod_prevs/DDTT.webp" alt="Software Thumbnail">
+    </div>
+    <div class="title-container">
+      <p>DOKI DOKI TRIPLE TROUBLE</p>
+    </div>
+    <div class="author-container">
       <div class="author-names">
-        <p>Author(s): JayQilin</p>
+        <p>Author(s): JQilin Gaming</p>
       </div>
       <div class="contributors">
-        <p>Contributors: Tune Eternal</p>
+        <p>Contributors: JQilin Gaming, Tune Eternal, Amigo, Kennedy Productions, The JQ Crew, StormBlaze, Proxyl_, Polygonsexual</p>
       </div>
     </div>
     <div class="description">
-      <p>Description: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut gravida justo. Nullam ut dolor vitae ex lacinia finibus. Mauris sodales libero id ligula tempor bibendum.</p>
+      <p>Doki Doki Triple Trouble is the first ever 18+ RomCom that centers around everyone's favorite pink-haired heartbreaker: Natsuki! Follow MC's year long escapade to try to be the main man in Natsuki's life as he competes with a different man... and then a different man... and then a different man. An original story jam packed with action, drama, comedy, romance, an incredible original soundtrack, and enough NSFW to make your head spin! A DDLC mod that will make you laugh, cry, question life, and jam out while doing so. Will MC surrender his common sense to get with Natsuki or will he realize that it's not worth the trouble? Find out now in Doki Doki Triple Trouble!</p>
     </div>
+    <h2 style = "text-align: center; margin-top: 20px; font-size: 2em;">DOWNLOAD HERE!</h2>
     <div class="download-links">
-      <h2>Download</h2>
       <ul>
-        <li><a href="#">Version 1.0</a></li>
-        <li><a href="#">Version 2.0</a></li>
+        <li><a href="#" target="_blank">- Version 1.0</a></li>
         <!-- Add more versions as needed -->
       </ul>
     </div>
@@ -35,28 +39,64 @@ export default {
   display: flex;
   flex-direction: column;
   align-items: center;
+  font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
 }
 
 .thumbnail-container {
-  margin-top: 160px;
-  position: relative;
-}
-
-.thumbnail-container img {
-  width: 200px; /* Adjust the width as needed */
-  height: auto;
   position: absolute;
   top: 0;
   left: 0;
+  margin-top: 100px;
+  margin-left: 50px;
 }
 
-.author-names,
+.thumbnail-container img {
+  width: 720px; /* Adjust the width as needed */
+  height: auto;
+}
+
+.title-container {
+  position: absolute;
+  top: 0;
+  right: 0;
+  margin-top: 200px;
+  font-size: 4em;
+  margin-right: 80px;
+  font-style: italic;
+}
+
+.author-container {
+  position: absolute;
+  top: 0;
+  right: 0;
+  margin-top: 400px;
+  font-size: 2em;
+  margin-right: 80px;
+}
+
+.author-names{
+  margin-bottom: 10px;
+}
 .contributors {
   margin-bottom: 10px;
+  font-size: 0.45em
+}
+
+.description {
+  margin-top: 550px;
+  margin-bottom: auto;
+  text-align: center;
+  font-size: 1.8em;
+  font-style: italic;
+  margin-left: 50px;
+  margin-right: 50px;
 }
 
 .download-links {
   text-align: center;
+  margin-top: 20px;
+  margin-bottom: 20px;
+  font-size: 2em;
 }
 
 .download-links ul {
